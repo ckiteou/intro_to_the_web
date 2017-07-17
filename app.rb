79f89d6488106c4af44de98d635c331d711dd63c
Costas Kiteou <ckiteou22@gmail.com>
2017-07-17 16:22:58 +0100

    Add index.erb file in view dir. Add ERB functionality

--- app.rb
+++ app.rb
@@ -1,20 +1,6 @@
 require 'sinatra'
 set :session_secret, 'super secret'
 
-get '/' do
-  "Hello World"
-end
-
-get '/secret' do
-  "This is a secret"
-end
-
-get '/cool stuff' do
-  "This is cool stuff"
-end
-
 get '/cat' do
-  "<div style='border: 3px dashed red'>
-  <img src='http://bit.ly/1eze8aE'>
-  </div>"
+  erb(:index)
 end
